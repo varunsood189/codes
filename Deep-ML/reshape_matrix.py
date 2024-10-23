22354edbbb760d77f7d4390625537752e528c2d4
varun <varunsood189@gmail.com>
2024-10-23 18:39:50 +0530

    Update reshape_matrix.py

--- Deep-ML/reshape_matrix.py
+++ Deep-ML/reshape_matrix.py
@@ -4,4 +4,4 @@ def reshape_matrix(a: list[list[int|float]],\
 				   new_shape: tuple[int, int]) -> list[list[int|float]]:
 	#Write your code here and return a python list after reshaping by using numpy's tolist() method
 	reshaped_matrix = np.array(a).reshape(new_shape)
-	return reshaped_matrix.to_list()
+	return reshaped_matrix.tolist()
