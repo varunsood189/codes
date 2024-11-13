a1b4b9e511b31eae88bbf2954a58e66d2112c5d0
varun <varunsood189@gmail.com>
2024-11-13 22:05:50 +0530

    Update decision_tree.py
    complete :)

--- Deep-ML/decision_tree.py
+++ Deep-ML/decision_tree.py
@@ -24,10 +24,8 @@ from collections import Counter
 import math
 def Entropy(label):
     label_counter = Counter(label)
-    if "Yes" in label_counter:
-        px = label_counter["Yes"]/len(label)
-        if px :    return -px*np.log2(px)
-    return 0
+    prob = [ label_counter[l]/len(label) for l  in label_counter]
+    return sum([-px*np.log2(px) for px in prob])
     
 def Information_Gain(examples,attr,target_attr): 
     total_entropy=Entropy([label[target_attr] for label in examples ])
@@ -44,18 +42,30 @@ def Information_Gain(examples,attr,target_attr):
     return total_entropy
 
 def learn_decision_tree(examples: list[dict], attributes: list[str], target_attr: str) -> dict:
-	# select the att which gives the best reduction in entropy and continue
+
+    if all([example[target_attr]==examples[0][target_attr] for example in examples ]):
+        return examples[0][target_attr]
     if len(attributes)==0:
-        return ""
+        return Counter([example[target_attr] for example in examples ]).most_common(1)[0][0]
     ig = {}
     for attr in attributes:
         ig[attr]= [Information_Gain(examples,attr,target_attr)]
-    print(max(ig))
-    decision_tree={}
+    best_attr = max(ig,key=ig.get)
+    decision_tree = {best_attr: {}}
+
+    dict_labels={}
+    attributes_copy = attributes.copy()
+    attributes_copy.remove(best_attr)
+    for data in examples:
+        if data[best_attr] in dict_labels:
+            dict_labels[data[best_attr]]+=[data]    
+        else:
+            dict_labels[data[best_attr]]=[data]  
+    for l in list(dict_labels.keys()):
+        subtree =  learn_decision_tree(dict_labels[l],attributes_copy,target_attr)
+        decision_tree[best_attr][l] = subtree
     return decision_tree
     
 # print(Entropy([label[target_attr] for label in examples ]))
 # print(Information_Gain(examples,'Outlook',target_attr))
 print(learn_decision_tree(examples, attributes, target_attr))
-
-
