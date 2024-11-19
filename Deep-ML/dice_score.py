d744873644ef01d223420f84a0065e858a687222
varun <varunsood189@gmail.com>
2024-11-19 22:20:25 +0530

    Update dice_score.py

--- Deep-ML/dice_score.py
+++ Deep-ML/dice_score.py
@@ -2,6 +2,8 @@ import numpy as np
 
 def dice_score(y_true, y_pred):
 	# Write your code here
+	if sum(y_true)==0 and sum(y_pred)==0:
+		return 0.0
 	tp = sum((y_true==1) & (y_pred==1))
 	fp =  sum((y_true==0) & (y_pred==1))
 	fn =  sum((y_true==1) & (y_pred==0))
